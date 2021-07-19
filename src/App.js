ca6c7b1a4369b50937b4b267c6057525690d927f
earnArtittiya <artittiya@smilefokus.com>
2021-07-19 18:42:12 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // in src/App.js
 import * as React from "react";
 import { Admin, Resource } from "react-admin";
-import {UserList}from "./Users"
+import UserList from "./Users";
 import jsonServerProvider from "ra-data-json-server";
 import { PostList, PostEdit, PostCreate } from "./Posts";
 import PostIcon from "@material-ui/icons/Book";
@@ -9,20 +9,22 @@ import UserIcon from "@material-ui/icons/Group";
 import Dashboard from "./Dashboard";
 import AuthProvider from "./AuthProvider";
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-const App = () => (
-  <Admin
-    dashboard={Dashboard}
-    authProvider={AuthProvider}
-    dataProvider={dataProvider}
-  >
-    <Resource name="users" list={UserList} icon={UserIcon} />
-    <Resource
-      name="posts"
-      list={PostList}
-      edit={PostEdit}
-      create={PostCreate}
-      icon={PostIcon}
-    />
-  </Admin>
-);
+function App() {
+  return (
+    <Admin
+      dashboard={Dashboard}
+      authProvider={AuthProvider}
+      dataProvider={dataProvider}
+    >
+      <Resource name="users" list={UserList} icon={UserIcon} />
+      <Resource
+        name="posts"
+        list={PostList}
+        edit={PostEdit}
+        create={PostCreate}
+        icon={PostIcon}
+      />
+    </Admin>
+  );
+}
 export default App;
--- src/Users.js
+++ src/Users.js
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { List, Datagrid, TextField, EmailField} from 'react-admin';
+import { List, Datagrid, TextField, EmailField } from "react-admin";
 import MyUrlField from "./MyUrlField";
 
-export const UserList = props => (
+export default function UserList(props) {
+  return (
     <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <EmailField source="email" />
-            <TextField source="phone" />
-            <MyUrlField source="website" />
-            <TextField source="company.name" />
-        </Datagrid>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" />
+        <EmailField source="email" />
+        <TextField source="phone" />
+        <MyUrlField source="website" />
+        <TextField source="company.name" />
+      </Datagrid>
     </List>
-);
+  );
+}
